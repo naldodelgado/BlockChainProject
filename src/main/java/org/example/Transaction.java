0fce05bffb566a40557cc0ca7573a308be374488
Naldo Delgado <nnmd210@gmail.com>
2024-03-05 20:05:29 +0000

    bouncy castle is now used to calculate the TransactionID

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -2,25 +2,37 @@ package org.example;
 
 import java.io.Serializable;
 import java.util.Date;
+import org.bouncycastle.crypto.digests.SHA1Digest;
 
 public class Transaction implements Serializable {
-    private String transactionId;
+    private byte[] transactionId;
     private String senderAddress;
     private String recipientAddress;
-    private double amount;
+    private int amount;
     private long timestamp;
-    private double transactionFee;
-    private String signature;
-    private String[] inputs;
-    private String[] outputs;
+
 
     // Constructor
-    public Transaction(String senderAddress, String recipientAddress, double amount) {
+    public Transaction(String senderAddress, String recipientAddress, int amount) {
         this.senderAddress = senderAddress;
         this.recipientAddress = recipientAddress;
         this.amount = amount;
         this.timestamp = new Date().getTime();
+        this.transactionId = generateTransactionId();
+        assert transactionId.length == 20;
         // Generate transactionId, signature, nonce, etc.
+    }
+
+    private byte[] generateTransactionId() {
+        // Use SHA-1 as an example hash function
+        SHA1Digest digest = new SHA1Digest();
+        byte[] data = (senderAddress + recipientAddress + amount + timestamp).getBytes();
+        byte[] hash = new byte[digest.getDigestSize()];
+
+        digest.update(data, 0, data.length);
+        digest.doFinal(hash, 0);
+
+        return hash;
     }
 
     // Getters and setters
