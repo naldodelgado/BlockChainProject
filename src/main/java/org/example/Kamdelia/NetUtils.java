0cf899b91240e066a39fd9600abe148fa5e42d9b
Ricardo <ricardo.cruz200230@gmail.com>
2024-04-27 00:02:15 +0100

    fixed bugs in NetUtils.java

--- src/main/java/org/example/Kamdelia/NetUtils.java
+++ src/main/java/org/example/Kamdelia/NetUtils.java
@@ -1,5 +1,8 @@
 package org.example.Kamdelia;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class NetUtils {
 
     public static String IPtoString(byte[] ip) {
@@ -9,12 +12,16 @@ public class NetUtils {
     public static byte[] IPfromString(String value) {
         byte[] ip = new byte[4];
 
+        Logger.getLogger(NetUtils.class.getName()).info("fjdbgfn" + value);
+
         String[] values = value.split("\\.");
+
+        Logger.getLogger(NetUtils.class.getName()).info("rjgndb" + Arrays.toString(values));
 
         assert values.length == 4;
 
         for (int i = 0; i < ip.length; i++) {
-            ip[i] = Byte.parseByte(values[i]);
+            ip[i] = (byte) Integer.parseInt(values[i]);
         }
 
         return ip;
