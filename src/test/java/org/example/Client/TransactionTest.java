f01bdd142e2ea77b69078d2673d63a3c2d8358ca
Ricardo <ricardo.cruz200230@gmail.com>
2024-05-10 19:32:35 +0100

    added tests

--- src/test/java/org/example/Client/TransactionTest.java
+++ src/test/java/org/example/Client/TransactionTest.java
@@ -1,12 +1,15 @@
 package org.example.Client;
 
+import org.example.Utils.KeysManager;
 import org.junit.Test;
 
 public class TransactionTest {
 
     @Test
     public void TestToAuctionGrpc() {
-        Auction a = new Auction(new byte[]{1, 2, 3}, 1, 1, 1, new byte[]{1, 2, 3}, new byte[]{1, 2, 3}, new byte[]{1, 2, 3});
+        var key = KeysManager.generateKeys();
+
+        Auction a = new Auction(new byte[]{1, 2, 3}, 1, 1, 1, key.getPublic(), new byte[]{1, 2, 3}, new byte[]{1, 2, 3});
 
         Auction b = (Auction) Transaction.fromGrpc(a.toGrpc());
 
@@ -15,7 +18,9 @@ public class TransactionTest {
 
     @Test
     public void TestToBidGrpc() {
-        Bid a = new Bid(new byte[]{1, 2, 3}, new byte[]{1, 2, 3}, new byte[]{1, 2, 3}, 1, new byte[]{1, 2, 3});
+        var key = KeysManager.generateKeys();
+
+        Bid a = new Bid(new byte[]{1, 2, 3}, key.getPublic(), key.getPublic(), 1, key.getPrivate());
 
         Bid b = (Bid) Transaction.fromGrpc(a.toGrpc());
 
