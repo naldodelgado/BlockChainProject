153c2fff32fe2be659d4b29e84c93828b349b354
Ricardo <ricardo.cruz200230@gmail.com>
2024-05-11 01:20:35 +0100

    added tests

--- src/test/java/org/example/Client/TransactionTest.java
+++ src/test/java/org/example/Client/TransactionTest.java
@@ -37,4 +37,34 @@ public class TransactionTest {
         assert a.equals(b);
     }
 
+    @Test
+    public void TestToAuctionGrpc2() {
+        var key = KeysManager.generateKeys();
+
+        Auction a = new Auction(new byte[]{1, 2, 3}, 1, 1, 1, key.getPublic(), new byte[]{1, 2, 3}, new byte[]{1, 2, 3});
+
+        Auction b = (Auction) Transaction.fromGrpc(a.toGrpc(new byte[]{1}));
+
+        assert a.equals(b);
+    }
+
+    @Test
+    public void TestToBidGrpc2() {
+        var key = KeysManager.generateKeys();
+
+        var key2 = KeysManager.generateKeys();
+
+        assert !Arrays.equals(key2.getPublic().getEncoded(), key.getPublic().getEncoded());
+
+        Bid a = new Bid(new byte[]{1, 2, 3}, key.getPublic(), key2.getPublic(), 1, key.getPrivate());
+
+        Bid b = (Bid) Transaction.fromGrpc(a.toGrpc(new byte[]{1}));
+
+        System.out.println(a);
+
+        System.out.println(b);
+
+        assert a.equals(b);
+    }
+
 }
